11f70e94e4f89646755f29de404c9fa7e4e7fb10
shangyuan191 <p76124752@gs.ncku.edu.tw>
2024-11-13 02:26:27 +0800

    cp torch_frame code into repo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import csv
+import sys
 import numpy as np
 import pandas as pd
 import argparse
@@ -19,6 +20,11 @@ from torch_frame.transforms import CatToNumTransform, MutualInformationSort
 from tqdm import tqdm
 import json
 import xlsxwriter
+import sys
+import os
+
+# 將 project_root 路徑插入到 sys.path 的最前面
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'torch_frame')))
 
 if __name__=="__main__":
     dataset_sizes=['small','large']
@@ -43,7 +49,23 @@ if __name__=="__main__":
                 parser.add_argument('--epochs', type=int, default=200)
                 parser.add_argument('--compile', action='store_true')
                 args = parser.parse_args()
-                df=pd.read_csv(f'./datasets/{dataset_size}_datasets/{task_type}/{dataset_name}/{dataset_name}.csv')
+                train_val_test_split_ratio=[0.64,0.16,0.2]
+                csv_path=f'./datasets/{dataset_size}_datasets/{task_type}/{dataset_name}/{dataset_name}.csv'
+                df=pd.read_csv(csv_path)
+                dataset = Yandex(df=df, name=args.dataset,train_val_test_split_ratio=train_val_test_split_ratio,task_type=task_type)
+                dataset.materialize()
+                print(dataset.df)
+                print(len(dataset))
+                print(type(dataset))
+                train_dataset, val_dataset, test_dataset = dataset.split()
+                print(len(train_dataset))
+                print(type(train_dataset))
+                print(len(val_dataset))
+                print(type(val_dataset))
+                print(len(test_dataset))
+                print(type(test_dataset))
+                
+
 
                 
 
